28438ed5242f759244e5967c063513b5055deffe
tjdwill <118497355+tjdwill@users.noreply.github.com>
2022-11-27 13:03:13 -0600

    Add files via upload
    Initial commit

---
+++ pyduino2.ino
@@ -0,0 +1,410 @@
+/*
+ * Author: Terrance Williams
+ * Date: 23 November 2022
+ * Description: A program that controls the Lynxmotion Control
+ * 3DoF Robot while interacting with Python Serial Communication.
+ */
+
+#include <LSS.h>
+
+// Notes for the User:
+/*
+ * 2IO LED codes : 0=Off (black); 
+ * 1=Red 2=Green; 3=Blue; 4=Yellow; 5=Cyan; 6=Magenta; 7=White; 
+ * Change color via (SoftwareSerial_object).write("#207LED<COLORCODE>\r")
+ * Leave the "<>" out when transmitting.
+ */
+
+ /*
+  * LSS ANGLE RANGES (in tenths of a degree)
+  * 
+  * Servo 1: -850 to 1250
+  * Servo 2: -750 to 200
+  * Servo 3: -450 to 600
+  * 
+  */
+
+
+// Globals
+#define ARRAYSIZE (3)
+#define SERIAL_DELAY (100)
+  // Servo variables
+#define GRIPPER_ID (209)
+#define GRIPPER_OPEN (450) //Just for visual use. MANUAL SEND COMMAND: #209D450
+#define GRIPPER_CLOSED (1500) //MANUAL COMMAND: (SoftwareSerial_object).write(#209D1800\r)
+//#define GRIPPER_LIMP #209L\r
+
+  // LSS Variables
+#define LSS_ID1    (1)
+#define LSS_ID2    (2)
+#define LSS_ID3    (3)
+#define LSS_BAUD  (LSS_DefaultBaud)  //115200
+#define MAX_ANGLE_DIFF (100)
+#define SLOW_MOVE_RATE (2) //ms per tenth of a degree. Ex. moving 500 units should take 1000 ms 
+// Create Objects
+SoftwareSerial LSS_Serial(8, 9);  //makes arduino pins 8 and 9 the serial Rx and Tx for the LSS bus.
+LSS myLSS1 = LSS(LSS_ID1); // Servos on the Lynxmotion Robot Arm (ID1 = Base rotation)
+LSS myLSS2 = LSS(LSS_ID2); // -angle values move end effector down (CCW servo rotation); wider range of motion (-750 to 200)
+LSS myLSS3 = LSS(LSS_ID3); // -angle values move end effector back (contraction)
+LSS gripper = LSS(GRIPPER_ID); //interact with gripper using LSS Protocol.
+/* declare an array populated with dummy values for positions
+*FORM: [[RED], [YELLOW], [GREEN],[BLUE]]
+* For example, if positions = [[P1],[P2],[HOME],[P3]],
+* P1 is associated with red, P2:yellow, P3:Blue, 
+* and HOME:Green
+*/
+int positions[4][3] = {{10, 10, 10},{11,11,11},{12,12,12},{13,13,13}}; 
+// Define Positions
+//int RESET[ARRAYSIZE] = {0, 0, 0}; // Robot reset config
+int pos_1[ARRAYSIZE] = {-404, -390, 275};//(16, 13.6, 5.5) mag 21cm // (17, 8.49, 5.5) = {-265, -416, 208} angles (x, y mag is 19)
+int pos_2[ARRAYSIZE] = {-205, -274, 405};  //(24, 9, 7)cm
+int pos_3[ARRAYSIZE] = {0, -416, 208}; //(19, 0, 5.5)cm
+int HOME[ARRAYSIZE] =  {337,-442, 191};// (15, -10, 5.5)cm Object home position
+int target_color = 10; // destination
+int current_color = 2; // current object pos (default to GREEN)
+
+// Variables for Python Communication 
+bool readyForInput = true;
+int incomingByte = -2; // a variable to read incoming serial data into
+int fill_cnt = 0; 
+char dumBuffer[8];
+
+void setup() {
+
+  /* Lynxmotion Setup
+   *  1. Start Serial Bus (LSS::initBus(SoftwareSerial object, Baud Rate)
+   *  2. Initialize Gripper
+   *  3. Move to RESET position + Gripper close
+  */
+  delay(5000);
+  
+  LSS::initBus(LSS_Serial, LSS_BAUD);
+  delay(500);
+  LSS_Serial.write("#207LED5\r");  //2IO should now be cyan.
+  delay(50);
+    
+  delay(3000);
+  resetLynx();
+  gripper.move(GRIPPER_CLOSED);
+  delay(250);
+  gripper.limp();
+  delay(50);
+  LSS_Serial.write("#207LED0\r");
+
+  // ESTABLISH Python Comms
+  Serial.begin(19200); //Python Communication Baudrate
+  delay(30); // give time to connect with python
+  while (!Serial){
+    ;
+  }
+  Serial.print("LYNX: Associate Colors.\n");
+  delay(SERIAL_DELAY*10);
+  Serial.print('I'); // tell Python we're ready
+  // populate positions array: WHAT COLOR is associated with the position?
+  while(fill_cnt != 4){
+    
+    // listen to COM port for data
+    if(Serial.available()>0){
+      
+      incomingByte = Serial.read();
+      // get rid of excess characters
+      if(Serial.peek() == '\r'){
+        Serial.readBytes(dumBuffer, 2);
+      }
+      else if(Serial.peek() == '\n'){
+        Serial.readBytes(dumBuffer, 1); 
+      }
+      delay(SERIAL_DELAY);
+      
+      // Associate Positions and Colors
+      if(incomingByte>='0' and incomingByte<='3'){
+        incomingByte = cvtASCII(incomingByte);
+        if(fill_cnt == 0) // Set position 1's color
+        {
+          populateArray(positions, pos_1, incomingByte);
+          Serial.print('I');
+          delay(SERIAL_DELAY);
+        }
+        if(fill_cnt == 1){
+          populateArray(positions, pos_2, incomingByte);
+          Serial.print('I');
+          delay(SERIAL_DELAY); 
+        }
+        if(fill_cnt == 2){
+          populateArray(positions, pos_3, incomingByte);
+          Serial.print('I');
+          delay(SERIAL_DELAY);
+        }
+        if(fill_cnt == 3){
+          populateArray(positions, HOME, incomingByte);
+          Serial.print('I');
+          delay(SERIAL_DELAY); 
+        }
+        fill_cnt++;
+        incomingByte = -2; // reset the byte
+      }
+    }   
+  }
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  // listen to COM port for data
+  if(Serial.available()>0){
+    
+    delay(SERIAL_DELAY); 
+    
+    // get the data
+    incomingByte = Serial.read();
+    // remove excess data from buffer
+    if(Serial.peek() == '\r'){
+       Serial.readBytes(dumBuffer, 2);
+      }
+    else if(Serial.peek() == '\n'){
+      Serial.readBytes(dumBuffer, 1);
+    }
+    delay(SERIAL_DELAY);
+
+    // treat the data
+    if(incomingByte>='0' and incomingByte<='3') // Did we get a color?
+    {
+      //getPos(current_pos);
+      target_color = cvtASCII(incomingByte);
+
+      //Pick & Place operation
+      if(target_color != current_color)
+      {
+        moveArm(positions[target_color]);  //PICK&PLACE;
+        current_color = target_color;
+      }
+      else
+      {
+        ;
+      }      
+      // variable reset
+      target_color = 10;  
+      Serial.print("I"); // Open to comms
+      delay(SERIAL_DELAY);
+    }
+    else if(incomingByte != 'q') // Not a target color but also not the 'QUIT' signal; Tell py to send new data.
+    {
+      Serial.print("I");
+      delay(SERIAL_DELAY); 
+    }
+    else // RECEIVED 'QUIT' SIGNAL FROM PYTHON
+    { 
+      Serial.print("9");
+      delay(SERIAL_DELAY);
+      
+        //MAKE A MOVE TO RESET POSITION + GO LIMP; PROGRAM IS DONE.
+       
+    }
+    // reset variables
+    incomingByte = -1;
+  }   
+}
+
+// HELPER FUNCTIONS 
+/*
+ * int cvtASCII (int input_character):
+ * Translates python data ['0','3'] to the integers [0,3].
+ * Used to iterate position arrays.
+ */
+int cvtASCII(int character){
+  if(character == '0'){
+    return 0;
+  }
+  if(character == '1'){
+    return 1;
+  }
+  if(character == '2'){
+    return 2;
+  }
+  if(character == '3'){
+    return 3;
+  }
+  else{
+    return -1;
+  }
+}
+
+
+/*
+ * void populateArray
+ * 
+ * Inputs the array meant to store the servo angles of the four positions, 
+ * the position array to copy, 
+ * and the color position to copy it to.
+ * 
+ * For example, void(positions, pos_1, 3) will result in position 1 being associated with the color BLUE such that:
+ * positions = [[], [], [], [pos_1]] = [[RED],[YELLOW],[GREEN],[BLUE]]
+ */
+
+void populateArray(int master[4][ARRAYSIZE], int entry[ARRAYSIZE], int place){
+  for(int i=0; i<ARRAYSIZE; i++){
+    master[place][i] = entry[i];
+  }
+}
+
+void moveArm(int target[3]){
+  // wake up LSSs
+  resetLynx();
+  delay(500);
+  // servo angles of target position
+  int obj_target1 = target[0];
+  int obj_target2 = target[1];
+  int obj_target3 = target[2];
+  // servo angles of current object position
+  
+  int obj_curr1 = positions[current_color][0];
+  int obj_curr2 = positions[current_color][1];
+  int obj_curr3 = positions[current_color][2];
+  
+  
+  //current servo angles
+  int32_t currS1 = myLSS1.getPosition();
+  delay(SERIAL_DELAY);
+  int32_t currS2 = myLSS2.getPosition();
+  delay(SERIAL_DELAY);
+  int32_t currS3 = myLSS3.getPosition();
+  delay(SERIAL_DELAY);
+  // CONSOLE Check: REMOVE FOR PYTHON OPERATION
+
+ /* PICK & PLACE
+  *
+  * MOVE ARM TO OBJECT (Open Gripper ,Move S1, Move S3, MV S2, Close Gripper) 
+  * INTERMEDIATE MOVEMENT (Move S2, Move S3): Basically, give the bot room to move
+  * MOVE ARM TO TARGET (Move S1, Move S3, Move S2)
+  * Release (Open Gripper (slowly))
+  * Intermediate Move 
+  * MOVE TO RESET
+  * GRIPPER LIMP
+  */
+  LSS_Serial.write("#207LED6\r");  //turn on 2IO LED to indicate movement
+  delay(SERIAL_DELAY);
+  gripper.move(GRIPPER_OPEN);
+  // move to object
+    // Servo1
+  if(abs(obj_curr1 - currS1) >= MAX_ANGLE_DIFF + 100){
+    myLSS1.moveT(obj_curr1, SLOW_MOVE_RATE*abs(obj_curr1 - currS1));
+    delay((SLOW_MOVE_RATE*abs(obj_curr1 - currS1))+150);
+  }
+  else{
+    myLSS1.move(obj_curr1);
+    delay(500);
+  }
+    // Servo 3
+  if(abs(obj_curr3 - currS3) >= MAX_ANGLE_DIFF){
+    myLSS3.moveT(obj_curr3, 3*abs(obj_curr3 - currS3)/2);
+    delay((SLOW_MOVE_RATE*abs(obj_curr3 - currS3))+150);
+  }
+  else{
+    myLSS3.move(obj_curr3);
+    delay(500);
+  }
+    // Servo 2
+  if(abs(obj_curr2 - currS2) >= MAX_ANGLE_DIFF){
+    myLSS2.moveT(obj_curr2, SLOW_MOVE_RATE*abs(obj_curr2 - currS2));
+    delay(2*(SLOW_MOVE_RATE*abs(obj_curr2 - currS2)));
+  }
+  else{
+    myLSS2.move(obj_curr2);
+    delay(500);
+  }
+  //grab object
+  gripper.move(GRIPPER_CLOSED);
+  delay(250);
+  // move to intermediate position
+  if(abs(obj_curr2) >= MAX_ANGLE_DIFF){
+    myLSS2.moveT(0, SLOW_MOVE_RATE*abs(0-obj_curr2));
+    delay(2*(SLOW_MOVE_RATE*abs(0-obj_curr2)));
+  }
+  else{
+    myLSS2.move(0);
+  }
+  if(abs(obj_curr3) >= MAX_ANGLE_DIFF){
+    myLSS3.moveT(0, SLOW_MOVE_RATE*abs(0-obj_curr3));
+    delay((SLOW_MOVE_RATE*abs(0-obj_curr3))+150);
+  }
+  else{
+    myLSS3.move(0);
+  }
+  // move to Target (S1->S3->S2)
+    // Servo 1
+  if (abs(obj_target1 - obj_curr1) >= MAX_ANGLE_DIFF){
+    myLSS1.moveT(obj_target1, SLOW_MOVE_RATE*abs(obj_target1 - obj_curr1));
+    delay((SLOW_MOVE_RATE*abs(obj_target1 - obj_curr1))+150); 
+  }
+  else{
+    myLSS1.move(obj_target1);
+    delay(500);
+  }
+  // servo 3
+  currS3 = myLSS3.getPosition();
+  delay(200);
+  if (abs(obj_target3-currS3) >= MAX_ANGLE_DIFF){
+    delay(200);
+    myLSS3.moveT(obj_target3, 3*abs(obj_target3-currS3)/2);
+    delay(SLOW_MOVE_RATE*abs(obj_target3));
+  }
+  else{
+    myLSS3.move(obj_target3);
+    delay(500);
+  }
+  // servo 2 (set object down slowly)
+  currS2 = myLSS2.getPosition();
+  delay(200);
+  myLSS2.moveT(obj_target2, SLOW_MOVE_RATE*abs(obj_target2-currS2));
+  delay(2*SLOW_MOVE_RATE*abs(obj_target2));
+  //
+  gripper.move(GRIPPER_OPEN);
+  delay(500);
+  resetLynx();
+  delay(250);
+  gripper.move(GRIPPER_CLOSED);
+  delay(200);
+  // gripper limp
+  gripper.limp();
+  delay(200);
+  LSS_Serial.write("#207LED0\r");
+  delay(200);
+  myLSS1.limp();
+  myLSS2.limp();
+  myLSS3.limp();
+}
+
+/*
+ * Move Robot to RESET position (all servo angles 0) and close gripper.
+ */
+void resetLynx(){
+  // get current servo positions & max angle
+  int32_t currS1 = myLSS1.getPosition();
+  delay(SERIAL_DELAY);
+  int32_t currS2 = myLSS2.getPosition();
+  delay(SERIAL_DELAY);
+  int32_t currS3 = myLSS3.getPosition();
+  delay(SERIAL_DELAY);
+  currS1 = abs(currS1);
+  currS2 = abs(currS2);
+  currS3 = abs(currS3);
+  
+  // make moves  
+  if (abs(currS2) >= MAX_ANGLE_DIFF){
+    myLSS2.moveT(0, SLOW_MOVE_RATE*currS2);
+    delay(2*SLOW_MOVE_RATE*currS2);
+  }
+  if (abs(currS3) >= MAX_ANGLE_DIFF){
+    myLSS3.moveT(0, (3*currS3)/2);
+    delay(2*SLOW_MOVE_RATE*currS3);
+  }
+  if (abs(currS1) >= MAX_ANGLE_DIFF){
+    myLSS1.moveT(0, SLOW_MOVE_RATE*currS1);
+    delay(2*SLOW_MOVE_RATE*currS1);
+  }
+  else{
+    myLSS2.move(0);
+    myLSS3.move(0);
+    myLSS1.move(0);
+    delay(1000);
+  }
+}
---
+++ python2lynxbot.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 15:28:32 2022
+
+@title: Python2Lynxbot
+@author: Terrance Williams
+@date: 10 November 2022
+@description: Python program that handles color detection and passes color data
+over Serial Port to Arudino.
+"""
+
+
+# Imports
+import cv2 as cv
+import numpy as np
+from time import sleep
+import serial
+import sys
+
+# Global(s)
+global SERIAL_SLEEP
+
+
+# Functions
+
+'''
+handleSerErr:
+Allows program to exit upon disconnecting from serial line.    
+    
+'''
+
+
+def handleSerErr():
+    arduino.__del__()  # close port
+    cv.destroyAllWindows()
+    cap.release()  # Release Camera
+    sys.exit("PYTHON: Connection with LYNX broken.")
+    
+    
+def img_mask(image):
+    # input raw image
+    # outputs array of masked images (red, yellow, green, blue, original)
+    img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+    # Generate HSV Thresholds
+    red_thresh1 = np.array([[0, SATURATION_LOWER, BRIGHTNESS_LOWER],
+                            [7, SATURATION_MAX, BRIGHTNESS_MAX]])
+    red_thresh2 = np.array([[165, SATURATION_LOWER, BRIGHTNESS_LOWER],
+                            [179, SATURATION_MAX, BRIGHTNESS_MAX]])
+    green_thresh = np.array([[40, SATURATION_LOWER, BRIGHTNESS_LOWER],
+                             [90, SATURATION_MAX, BRIGHTNESS_MAX]])
+
+    blue_thresh = np.array([[100, SATURATION_LOWER, BRIGHTNESS_LOWER],
+                            [140, SATURATION_MAX, BRIGHTNESS_MAX]])
+    yellow_thresh = np.array([[20, SATURATION_LOWER, BRIGHTNESS_LOWER],
+                              [35, SATURATION_MAX, BRIGHTNESS_MAX]])
+
+    # Generate Masks
+    red_mask1 = cv.inRange(img_hsv, red_thresh1[0], red_thresh1[1])
+    red_mask2 = cv.inRange(img_hsv, red_thresh2[0], red_thresh2[1])
+    # Combine red masks
+    red_mask = cv.bitwise_or(red_mask1, red_mask2, mask=None)
+
+    green_mask = cv.inRange(img_hsv, green_thresh[0], green_thresh[1])
+    blue_mask = cv.inRange(img_hsv, blue_thresh[0], blue_thresh[1])
+    yellow_mask = cv.inRange(img_hsv, yellow_thresh[0], yellow_thresh[1])
+
+    yellow_masked = cv.bitwise_and(image, image, mask=yellow_mask)
+    red_masked = cv.bitwise_and(image, image, mask=red_mask)
+    blue_masked = cv.bitwise_and(image, image, mask=blue_mask)
+    green_masked = cv.bitwise_and(image, image, mask=green_mask)
+
+    output = np.array([red_masked, yellow_masked,
+                       green_masked, blue_masked])
+
+    return output
+
+
+def colorDetect()->int:
+    consec_detections = 0  # Tracks how many consecutive images detect a given color
+    while True:
+        _, img = cap.read()
+        img = cv.flip(img, 1)
+
+        # =============================================================================
+        #     Detecting a color in a subregion:
+        #     1. Draw rectangle around desired subregion
+        #     2. Draw circle in center of subregion. The center of this circle is the
+        #     pixel we will analyze.
+        #     3. Segment the frame into a variable consisting solely of the subregion
+        #     4. Apply relevant masking and filtering
+        #     5. Perform bitwise AND operation to isolate the desired color.
+        #     6. If center pixel is not black, that color is present (in theory).
+        #
+        # =============================================================================
+
+        # 1. Draw rectangle
+        height, width, _ = img.shape
+        rect_TpLft = (0, int(7 * height / 8))
+        rect_BtmRght = (int(width / 4), height)
+        rect_color = (0, 255, 0)
+        rect_thickness = 2
+        image2 = np.copy(img)  # so we don't modify the original
+        image2 = cv.rectangle(image2, rect_TpLft, rect_BtmRght, rect_color,
+                              rect_thickness)
+        # 2. Draw circle
+        rect_center = (int(rect_BtmRght[0] / 2), int((rect_TpLft[1] + height) / 2))
+        circle_rad = 20
+        circle_color = (255, 0, 255)
+        circle_thickness = 5
+        image2 = cv.circle(image2, rect_center, circle_rad, circle_color, circle_thickness)
+        # print(rect_TpLft, rect_center, rect_BtmRght)
+        # print(f'Center BGR is: {img[rect_center[-1], rect_center[0]]}')
+
+        # 3. Define subregion and relevant dimensions
+
+        subregion = img[int(7 * height / 8):height, 0:int(width / 4)]
+        sub_height, sub_width, _ = subregion.shape
+        # sub_center = subregion[int(sub_height / 2), int(sub_width / 2)]
+        # print(sub_center)
+
+        # 4. Apply masks & 5. bitwise AMD
+        masked_imgs = img_mask(subregion)
+
+        # 6. Check center pixel and surrounding pixels to determine color sum
+
+        i = 0  # Track which image has the color
+        # Define center
+        y, x = int(sub_height / 2), int(sub_width / 2)
+        color_detected = False
+        for im in masked_imgs:
+            im_center = im[y, x]
+            # reach surrounding pixels from center
+
+            im_area = np.array([im[y - 1, x - 1], im[y - 1, x], im[y - 1, x + 1],
+                                im[y, x - 1], im_center, im[y, x + 1],
+                                im[y + 1, x - 1], im[y + 1, x], im[y + 1, x + 1]])
+            num_detections = 0
+            for pixel in im_area:
+                if np.sum(pixel) > 0:
+                    num_detections += 1
+                # print(f'Pixels with color on Image {i}: {num_detections}')
+            if num_detections == len(im_area):  # do all pixels have a non-black color?
+                # Color found
+                color_detected = True
+                break
+            i += 1
+        if color_detected:
+            consec_detections += 1
+            print(f'PYTHON: Consecutive Detections: {consec_detections}.')
+        else:
+            # print('PYTHON: No color found.')
+            consec_detections = 0
+        subregion = cv.circle(subregion, (x, y), 15, circle_color, 5)
+
+        # cv.imshow("Original", img)
+        # cv.imshow("Rectangle", image2)
+        cv.imshow("Subregion", subregion)
+        cv.moveWindow("Subregion", 0, 0)
+        cv.waitKey(1)
+        if consec_detections >= DETECTION_THRESH:
+            print(f'Color detected on Image {i}. Exiting.')
+            return i
+
+
+def send_color(pic_num: int, serial_line):
+    
+    if pic_num >= 0 and pic_num <= 3:
+        byteToSend = str(pic_num).encode()
+        try:
+            serial_line.write(byteToSend)
+        except serial.SerialException:
+            handleSerErr()
+        sleep(SERIAL_SLEEP)
+        print(f"Byte Sent: {byteToSend}\n")
+        sleep(3)  # Give human chance to see output
+    else:
+        print("Invalid data.\n")
+
+
+# Setup
+
+# Camera Stuff
+SATURATION_LOWER = 50
+SATURATION_MAX = 255  # Max 'S' value in HSV
+BRIGHTNESS_LOWER = 20
+BRIGHTNESS_MAX = 255  # Max 'V' Value in HSV
+DETECTION_THRESH = 110  # Number of consecutive detects required to be sure the color is present.
+
+# Set up webcam
+cap = cv.VideoCapture(0)
+
+# Serial
+BAUD_RATE = 19200
+SERIAL_TIMEOUT = 5  # (in seconds)
+serial_connected = False
+
+while not serial_connected:
+    try:
+        arduino = serial.Serial(port='COM10', baudrate=BAUD_RATE, timeout=SERIAL_TIMEOUT)
+        serial_connected = True
+    except serial.SerialException:
+        print("Waiting for COM to open.")
+        sleep(2)
+       
+SERIAL_SLEEP = 0.05  # HOW LONG (in seconds) to delay
+
+# Useful booleans        
+arduino_ready = False
+program_started = False
+
+"""Find a way to send a QUIT signal 'q'"""
+
+while not program_started:
+    print("PYTHON: Awaiting Start signal...")
+    try:
+        if arduino.in_waiting > 0:
+            dummy = arduino.read(24)
+            dummy = dummy.decode(encoding='ascii')
+            print(dummy)
+            if dummy == 'LYNX: Associate Colors.\n':
+                program_started = True
+                print("PYTHON: Starting Program.")
+    except serial.SerialException:
+        handleSerErr()
+    sleep(2)
+
+while True:
+    
+    if not arduino_ready:
+        #   check for serial data
+        try:
+            if arduino.in_waiting > 0:
+                check_data = arduino.read(1)
+                check_data = check_data.decode(encoding='ascii')
+                if check_data == 'I':
+                    arduino_ready = True
+                elif check_data == '9':
+                    print("PYTHON: Program Exit Received. Exiting...")
+                    break
+        except serial.SerialException:
+            handleSerErr()
+    else:
+        # color_detect
+        print("PYTHON: Entering Color Detection")
+        pic_num = colorDetect()
+        send_color(pic_num, arduino)
+        arduino_ready = False
